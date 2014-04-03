14918a13359a7dd077fc2f57121413f3602f7ecc
Bertrand Moulard <pair+translated@pivotallabs.com>
2014-04-03 17:09:50 -0400

    Add rails 3 test

---
+++ spec/integration/deploy_a_rails3_app_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'deploying a rails 3 application' do
+  it 'make the homepage available' do
+    Machete.deploy_app("rails3_mri_193", {
+      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
+      with_db: true
+    }) do |app|
+      expect(app.homepage_html).to include('hello')
+    end
+  end
+end
