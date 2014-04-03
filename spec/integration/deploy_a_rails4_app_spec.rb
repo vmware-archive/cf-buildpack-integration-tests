caa0c544c19ec9221109afeab280d05b2c63889a
Bertrand Moulard <pair+translated@pivotallabs.com>
2014-04-03 17:03:01 -0400

    Add options to rails 4 test

--- spec/integration/deploy_a_rails4_app_spec.rb
+++ spec/integration/deploy_a_rails4_app_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe 'deploying a rails 4 application' do
   it 'make the homepage available' do
-    Machete.deploy_app("rails4_web_app", "bundle exec rake db:migrate && bundle exec rails s -p $PORT") do |app|
+    Machete.deploy_app("rails4_web_app", {
+      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
+      with_db: true
+    }) do |app|
       expect(app.homepage_html).to include('The Kessel Run')
     end
   end
