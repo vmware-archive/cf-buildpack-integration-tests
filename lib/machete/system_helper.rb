3c62311716308bd0563d456e6e87855fd6fd2c4e
Rasheed Abdul-Aziz <squeedee@gmail.com>
2014-04-07 14:57:59 -0400

    command logging shows output from commands

--- lib/machete/system_helper.rb
+++ lib/machete/system_helper.rb
@@ -1,6 +1,8 @@
 module SystemHelper
   def run_cmd(cmd)
     puts "$ #{cmd}" if ENV['PRINT_COMMANDS']
-    `#{cmd}`
+    result = `#{cmd}`
+    puts result if ENV['PRINT_COMMANDS']
+    result
   end
 end
