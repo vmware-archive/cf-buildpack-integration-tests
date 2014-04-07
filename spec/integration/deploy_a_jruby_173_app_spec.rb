f8d49645e7bb9e25c691789ef69faf736ac65e1e
Bertrand Moulard and Jordi Noguera <pair+translated+jordi@pivotallabs.com>
2014-04-07 17:18:49 -0400

    Add pending jruby test

---
+++ spec/integration/deploy_a_jruby_173_app_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe 'deploying a jruby 1.7.3 application' do
+  xit "deploys a jruby 1.7.3 (legacy jdk) properly" do
+    Machete.deploy_app("ruby_193_jruby_173", {with_db: false, cmd: ''}) do |app|
+      expect(app.output).to match("Installing JVM: openjdk1.7.0_25")
+      expect(app.output).to match("ruby-1.9.3-jruby-1.7.3")
+      expect(app.output).not_to include("OpenJDK 64-Bit Server VM warning")
+      puts app.output
+    end
+  end
+end
