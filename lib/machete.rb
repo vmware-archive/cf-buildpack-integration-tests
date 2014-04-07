fb7c49eaba5fce193c6071cae4cce65cbe944d08
Bertrand Moulard and Jordi Noguera <pair+translated+jordi@pivotallabs.com>
2014-04-07 16:40:31 -0400

    Add default empty options hash to deploy_app

--- lib/machete.rb
+++ lib/machete.rb
@@ -2,7 +2,7 @@ require 'machete/logger'
 require 'machete/app'
 
 module Machete
-  def self.deploy_app(app_name, options, &block)
+  def self.deploy_app(app_name, options={}, &block)
     app = Machete::App.new(app_name, options[:cmd])
     app.push(options[:with_db])
     block.call(app)
