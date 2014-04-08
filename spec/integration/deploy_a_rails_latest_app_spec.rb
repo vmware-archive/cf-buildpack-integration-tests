220e382fe97746d7ee3ed29fb6fa779a9ca7ea96
Jordi Noguera and Rasheed Abdul-Aziz <pair+jordi+squeedee@pivotallabs.com>
2014-04-08 16:26:51 -0400

    enable latest ruby test

--- spec/integration/deploy_a_rails_latest_app_spec.rb
+++ spec/integration/deploy_a_rails_latest_app_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'deploying a rails 4 application' do
-  xit 'make the homepage available' do
+  it 'make the homepage available' do
     Machete.deploy_app("rails_latest_web_app", {
       cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
       with_db: true
