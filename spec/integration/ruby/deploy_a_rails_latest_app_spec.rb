7013a43221a49e935460328899793738db075bee
Bertrand Moulard <pair+translated@pivotallabs.com>
2014-04-10 09:41:15 -0400

    Temporarily disable flaky test

--- spec/integration/ruby/deploy_a_rails_latest_app_spec.rb
+++ spec/integration/ruby/deploy_a_rails_latest_app_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'deploying a rails 4 application' do
-  it 'make the homepage available' do
+  xit 'make the homepage available' do
     Machete.deploy_app("rails_latest_web_app", :ruby, {
       cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
       with_pg: true
