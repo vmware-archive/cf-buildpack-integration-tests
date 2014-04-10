4404dd39d24c3641ed84d3eb77efc67b1c19a7e1
Bertrand Moulard <pair+translated@pivotallabs.com>
2014-04-10 11:25:28 -0400

    Move ci commands to script

---
+++ scripts/ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+export PATH=$PATH:/Users/pivotal/.rvm/bin
+rvm use ruby-2.0.0-p451
+bundle install
+rm -rf tmp_buildpacks
+mkdir tmp_buildpacks
+git clone https://github.com/pivotal-cf-experimental/cf-buildpack-ruby tmp_buildpacks/cf-buildpack-ruby
+git clone https://github.com/pivotal-cf-experimental/cf-buildpack-go tmp_buildpacks/cf-buildpack-go
+cf target -o pivotal -s integration-tests
+export BUILDPACK_ROOT=tmp_buildpacks
+bundle exec rspec -f d && BUILDPACK_MODE=offline bundle exec rspec
