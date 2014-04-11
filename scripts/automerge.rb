c43966a892b56bf749d8b3afddc8218cb5e14a98
Bertrand Moulard <pair+translated@pivotallabs.com>
2014-04-11 11:54:26 -0400

    Flesh out the auto merge script

--- scripts/automerge.rb
+++ scripts/automerge.rb
@@ -1,13 +1,49 @@
 #!/usr/bin/env ruby
 
-`mkdir -p tmp/buildpacks`
 
-`git clone https://github.com/pivotal-cf-experimental/cf-buildpack-ruby tmp/buildpacks/cf-buildpack-ruby`
-`git clone https://github.com/pivotal-cf-experimental/cf-buildpack-go tmp/buildpacks/cf-buildpack-go`
+def push_merge
+  if system "cd tmp/buildpacks/cf-buildpack-ruby && git push origin master"
+    exit 0
+  else
+    puts "Could not push the merged ruby buildpack"
+    exit 1
+  end
 
-Dir.chdir("tmp/buildpacks/cf-buildpack-ruby")
+end
 
-`git remote add heroku-github https://github.com/heroku/heroku-buildpack-ruby`
-`git fetch heroku-github`
-puts `git merge heroku-github/master`
+def run_specs
+  if system "BUILDPACK_ROOT=tmp/buildpacks bundle exec rspec spec/integration/ruby"
+    push_merge
+  else
+    exit 1
+  end
+end
 
+begin
+  `rm -rf tmp`
+  `mkdir -p tmp/buildpacks`
+
+  `git clone https://github.com/pivotal-cf-experimental/cf-buildpack-ruby tmp/buildpacks/cf-buildpack-ruby`
+  `git clone https://github.com/pivotal-cf-experimental/cf-buildpack-go tmp/buildpacks/cf-buildpack-go`
+
+  Dir.chdir("tmp/buildpacks/cf-buildpack-ruby")
+
+  `git remote add heroku-github https://github.com/heroku/heroku-buildpack-ruby`
+  `git fetch heroku-github`
+  output = `git merge heroku-github/master`
+  Dir.chdir("../../..")
+
+  if output.match(/up-to-date/)
+    puts "Up to date, not doing anything"
+  elsif output.match(/CONFLICT/)
+    puts "Merge conflicts must be resolved manually"
+    exit 1
+  else
+    puts "Merge successful, running the tests"
+    run_specs()
+  end
+rescue StandardError => e
+  puts e
+  puts "Something went wrong during auto merge, investigate"
+  exit 1
+end
