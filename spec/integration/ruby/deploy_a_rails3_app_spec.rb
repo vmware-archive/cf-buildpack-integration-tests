291182537b796eb1f97c9eb9a12dff295b556537
Aaron Triantafyllidis and Bertrand Moulard <pair+atriantafyllidis+bertrandmoulard@pivotallabs.com>
2014-04-22 10:51:42 -0400

    Re-add the migrate command for test rails apps

--- spec/integration/ruby/deploy_a_rails3_app_spec.rb
+++ spec/integration/ruby/deploy_a_rails3_app_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'deploying a rails 3 application' do
   it 'make the homepage available' do
     Machete.deploy_app("rails3_mri_193", :ruby, {
+      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
       with_pg: true
     }) do |app|
       expect(app.homepage_html).to include('hello')
--- spec/integration/ruby/deploy_a_rails4_app_spec.rb
+++ spec/integration/ruby/deploy_a_rails4_app_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'deploying a rails 4 application' do
   it 'make the homepage available' do
     Machete.deploy_app("rails4_web_app", :ruby, {
+      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
       with_pg: true
     }) do |app|
       expect(app.homepage_html).to include('The Kessel Run')
