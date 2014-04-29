25ecc0b2f560ebaf83eb8da1d90f1f45393da325
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-04-29 11:08:01 -0400

    create org and space in CI if they don't exist

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,6 +7,9 @@ mkdir tmp_buildpacks
 git clone https://github.com/cloudfoundry/cf-buildpack-ruby tmp_buildpacks/cf-buildpack-ruby
 git clone https://github.com/pivotal-cf-experimental/cf-buildpack-go tmp_buildpacks/cf-buildpack-go
 git clone https://github.com/pivotal-cf-experimental/cf-buildpack-nodejs tmp_buildpacks/cf-buildpack-nodejs
+cf create-org pivotal
+cf target -o pivotal
+cf create-space integration-tests
 cf target -o pivotal -s integration-tests
 export BUILDPACK_ROOT=tmp_buildpacks
 bundle exec rspec -f d && BUILDPACK_MODE=offline bundle exec rspec
