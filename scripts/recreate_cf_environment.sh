075cf58f38a61ef41e6fa4654c2bcb571dc373b3
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-04-29 14:42:22 -0400

    recreate cf environment script added

---
+++ scripts/recreate_cf_environment.sh
@@ -0,0 +1,21 @@
+source "$HOME/.rvm/scripts/rvm"
+rvm use 1.9.3
+
+cd ~/workspace/bosh-lite
+
+vagrant destroy -f
+vagrant up --provider vmware_fusion
+bundle exec bosh target 192.168.50.4
+bundle exec bosh login admin admin
+scripts/add-route
+vagrant ssh -c "sudo sv restart director"
+bundle exec bosh upload stemcell latest-bosh-stemcell-warden.tgz
+cd ~/workspace/cf-release
+./update
+cd ~/workspace/bosh-lite
+./scripts/make_manifest_spiff
+cd ~/workspace/cf-release
+bundle exec bosh upload release dev_releases/cf-169.$1-dev.yml
+cd ~/workspace/bosh-lite
+bundle exec bosh deployment manifests/cf-manifest.yml
+bundle exec bosh -n deploy
