d5a5519eec152fc7efb622f1c44f64d61a91df5f
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-04-29 10:32:56 -0400

    Enabled Jruby spec, created sinatra jruby app to test against

--- spec/integration/ruby/deploy_a_jruby_173_app_spec.rb
+++ spec/integration/ruby/deploy_a_jruby_173_app_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
 describe 'deploying a jruby 1.7.3 application', :ruby_buildpack do
-  xit "deploys a jruby 1.7.3 (legacy jdk) properly" do
-    Machete.deploy_app("ruby_193_jruby_173", :ruby) do |app|
-      expect(app.output).to match("Installing JVM: openjdk1.7.0_25")
-      expect(app.output).to match("ruby-1.9.3-jruby-1.7.3")
+  it "deploys a jruby 1.7.3 (legacy jdk) properly" do
+    Machete.deploy_app("sinatra_jruby_web_app", :ruby) do |app|
+      expect(app.output).to match("Installing JVM")
+      expect(app.output).to match("ruby-1.8.7-jruby-1.7.8")
       expect(app.output).not_to include("OpenJDK 64-Bit Server VM warning")
       puts app.output
     end
