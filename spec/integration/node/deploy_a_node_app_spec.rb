7e02a373e859d08170764cc1fe6c22b05480f918
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-04-30 15:44:18 -0400

    rename hello_world to node_web_app

--- spec/integration/node/deploy_a_node_app_spec.rb
+++ spec/integration/node/deploy_a_node_app_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 describe 'deploying a nodejs app', :node_buildpack do
   it "makes the homepage available" do
-    Machete.deploy_app("hello_world", :nodejs, {
-      cmd: "node hello_world.js"
+    Machete.deploy_app("node_web_app", :nodejs, {
+      cmd: "node server.js"
     }) do |app|
       expect(app.homepage_html).to include "Hello, World!"
     end
