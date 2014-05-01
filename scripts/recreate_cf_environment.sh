dcae7409286727894785ca9015f38980f95ff448
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-05-01 10:34:50 -0400

    recreate script automatically creates release

--- scripts/recreate_cf_environment.sh
+++ scripts/recreate_cf_environment.sh
@@ -1,8 +1,11 @@
 source "$HOME/.rvm/scripts/rvm"
 rvm use 1.9.3
 
-cd ~/workspace/bosh-lite
+cd ~/workspace/cf-release
+rm -f dev_releases/*.yml
+bundle exec bosh create release
 
+cd ~/workspace/bosh-lite
 vagrant destroy -f
 vagrant up --provider vmware_fusion
 bundle exec bosh target 192.168.50.4
@@ -10,12 +13,16 @@ bundle exec bosh login admin admin
 scripts/add-route
 vagrant ssh -c "sudo sv restart director"
 bundle exec bosh upload stemcell latest-bosh-stemcell-warden.tgz
+
 cd ~/workspace/cf-release
 ./update
+
 cd ~/workspace/bosh-lite
 ./scripts/make_manifest_spiff
+
 cd ~/workspace/cf-release
-bundle exec bosh upload release dev_releases/cf-169.$1-dev.yml
+bundle exec bosh upload release dev_releases/cf-*.yml
+
 cd ~/workspace/bosh-lite
 bundle exec bosh deployment manifests/cf-manifest.yml
 bundle exec bosh -n deploy
