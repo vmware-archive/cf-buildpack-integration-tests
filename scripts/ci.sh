07f1a63fb82c69a1f61fb34b0408c165024814cd
Aaron Triantafyllidis and Rasheed Abdul-Aziz <pair+atriantafyllidis+squeedee@pivotallabs.com>
2014-05-01 10:48:35 -0400

    Use more noticable banners for CI script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,31 +1,31 @@
 #!/bin/bash -l
 
-echo
-echo "-----> Fetching CF CLI"
+echo "\n******* CI ***************************************************"
+echo "******* Fetching CF CLI"
 uname_lower=$(echo `uname` | awk '{print tolower($0)}')
 curl http://go-cli.s3.amazonaws.com/master/cf-$uname_lower-amd64.tgz | tar xzv
 
-echo
-echo "-----> Installing CF CLI on path"
+echo "\n******* CI ***************************************************"
+echo "******* Installing CF CLI on path"
 mkdir -p ./bin
 mv cf bin/
 export PATH=$PWD/bin:$PATH
 
-echo
-echo "-----> Adding VCAP ruby to path"
+echo "\n******* CI ***************************************************"
+echo "******* Adding VCAP ruby to path"
 export PATH=/var/vcap/packages/ruby/bin:$PATH
 
-echo
-echo "-----> Adding RVM and using Ruby 2.0"
+echo "\n******* CI ***************************************************"
+echo "******* Adding RVM and using Ruby 2.0"
 export PATH=/Users/pivotal/.rvm/bin:$PATH
 rvm use ruby-2.0.0-p451
 
-echo
-echo "-----> Bundling"
+echo "\n******* CI ***************************************************"
+echo "******* Bundling"
 bundle install
 
-echo
-echo "-----> Fetching buildpacks for testing"
+echo "\n******* CI ***************************************************"
+echo "******* Fetching buildpacks for testing"
 rm -rf tmp_buildpacks
 mkdir tmp_buildpacks
 git clone https://github.com/cloudfoundry/cf-buildpack-ruby tmp_buildpacks/cf-buildpack-ruby
@@ -33,18 +33,18 @@ git clone https://github.com/pivotal-cf-experimental/cf-buildpack-go tmp_buildpa
 git clone https://github.com/pivotal-cf-experimental/cf-buildpack-nodejs tmp_buildpacks/cf-buildpack-nodejs
 export BUILDPACK_ROOT=tmp_buildpacks
 
-echo
-echo "-----> Create CF org and space"
+echo "\n******* CI ***************************************************"
+echo "******* Create CF org and space"
 cf login -u admin -p admin
 cf create-org pivotal
 cf target -o pivotal
 cf create-space integration-tests
 cf target -o pivotal -s integration-tests
 
-echo
-echo "-----> Running online specs"
+echo "\n******* CI ***************************************************"
+echo "******* Running online specs"
 rspec -f d
 
-echo
-echo "-----> Running offline specs"
+echo "\n******* CI ***************************************************"
+echo "******* Running offline specs"
 BUILDPACK_MODE=offline rspec -f d
