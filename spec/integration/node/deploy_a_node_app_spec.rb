01e529348c838acef3a3ef2b3eec55fe7ecb0502
Aaron Triantafyllidis and Rasheed Abdul-Aziz <pair+atriantafyllidis+squeedee@pivotallabs.com>
2014-05-01 17:16:14 -0400

    Check if node app is staged

--- spec/integration/node/deploy_a_node_app_spec.rb
+++ spec/integration/node/deploy_a_node_app_spec.rb
@@ -5,6 +5,7 @@ describe 'deploying a nodejs app', :node_buildpack do
     Machete.deploy_app("node_web_app", :nodejs, {
       cmd: "node server.js"
     }) do |app|
+      expect(app).to be_staged
       expect(app.homepage_html).to include "Hello, World!"
     end
   end
