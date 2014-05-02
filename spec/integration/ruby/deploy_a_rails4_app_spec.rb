15b8d6984a99157efe6edf612c50f1652fbf8136
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-05-02 17:57:36 -0400

    Assert apps are staged once they are deployed

--- spec/integration/ruby/deploy_a_rails4_app_spec.rb
+++ spec/integration/ruby/deploy_a_rails4_app_spec.rb
@@ -6,6 +6,7 @@ describe 'deploying a rails 4 application', :ruby_buildpack do
       cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
       with_pg: true
     }) do |app|
+      expect(app).to be_staged
       expect(app.homepage_html).to include('The Kessel Run')
     end
   end
--- spec/integration/ruby/deploy_an_app_with_nokogiri_spec.rb
+++ spec/integration/ruby/deploy_an_app_with_nokogiri_spec.rb
@@ -4,6 +4,7 @@ describe "Bugs", :ruby_buildpack do
   context "MRI 1.8.7" do
     it "should install nokogiri" do
       Machete.deploy_app("mri_187_nokogiri", :ruby) do |app|
+        expect(app).to be_staged
         expect(app.output).to match("Installing nokogiri")
         expect(app.output).to match("Your bundle is complete!")
       end
