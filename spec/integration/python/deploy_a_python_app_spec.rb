cbbe0a57f186e532f836fe8f22ee298b26c6cb45
Aaron Triantafyllidis and Chris Haueter <pair+atriantafyllidis+chaueter@pivotallabs.com>
2014-05-07 16:13:04 -0400

    Vendor-enable python buildpack and vendor python dependencies
    [#70520198]

--- spec/integration/python/deploy_a_python_app_spec.rb
+++ spec/integration/python/deploy_a_python_app_spec.rb
@@ -1,10 +1,8 @@
 require "spec_helper"
 
 describe 'deploying a python web app', :python_buildpack do
-  xit "makes the homepage available" do
-    Machete.deploy_app("flask_web_app", :python, {
-      cmd: "python server.py"
-    }) do |app|
+  it "makes the homepage available" do
+    Machete.deploy_app("flask_web_app", :python) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "Hello, World!"
     end
