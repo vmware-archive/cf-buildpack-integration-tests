60aa71be1b7532dad7813405eae187735816758c
Aaron Triantafyllidis and Chris Haueter <pair+atriantafyllidis+chaueter@pivotallabs.com>
2014-05-07 17:18:07 -0400

    Test python app without vendored dependencies

--- spec/integration/python/deploy_a_python_app_spec.rb
+++ spec/integration/python/deploy_a_python_app_spec.rb
@@ -7,4 +7,13 @@ describe 'deploying a python web app', :python_buildpack do
       expect(app.homepage_html).to include "Hello, World!"
     end
   end
+
+  if Machete::BuildpackMode.online?
+    it "makes the homepage available" do
+      Machete.deploy_app("flask_web_app_no_dependencies", :python) do |app|
+        expect(app).to be_staged
+        expect(app.homepage_html).to include "Hello, World!"
+      end
+    end
+  end
 end
