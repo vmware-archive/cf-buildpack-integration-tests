be1a4bee2000924232a4bd097a67f657aebad531
Jordi Noguera <jordi@pivotallabs.com>
2014-05-15 15:13:53 -0400

    Use bosh-lite 1&2 instead of firewall script in specs

--- lib/machete/rspec_helpers.rb
+++ lib/machete/rspec_helpers.rb
@@ -3,30 +3,23 @@ require 'rspec/core'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    Machete::RSpecHelpers.setup
-  end
+    if Machete::BuildpackMode.offline?
+      Machete.logger.action 'Selecting OFFLINE BOSH Lite'
 
-  config.after(:suite) do
-    Machete::RSpecHelpers.teardown
-  end
-end
+      offline_api_target = ENV['OFFLINE_API_TARGET'] ? ENV['OFFLINE_API_TARGET'] : 'api.10.245.0.34.xip.io'
+      Machete.logger.info `cf api #{offline_api_target} --skip-ssl-validation`
+    else
+      Machete.logger.action 'Selecting ONLINE BOSH Lite'
 
-module Machete
-  module RSpecHelpers
-    class << self
-      def setup
-        return unless BuildpackMode.offline?
-
-        Machete.logger.action 'Bringing firewall up, bye bye internet'
-        Machete::Firewall.enable_firewall
-      end
-
-      def teardown
-        return unless BuildpackMode.offline?
-
-        Machete.logger.action 'Taking firewall down, internet is back'
-        Machete::Firewall.disable_firewall
-      end
+      online_api_target = ENV['ONLINE_API_TARGET'] ? ENV['ONLINE_API_TARGET'] : 'api.10.244.0.34.xip.io'
+      Machete.logger.info `cf api #{online_api_target} --skip-ssl-validation`
     end
+
+    Machete.logger.action 'Logging into BOSH lite'
+
+    Machete.logger.info `cf login -u admin -p admin`
+    Machete.logger.info `cf target -o pivotal -s integration`
   end
+
 end
+
