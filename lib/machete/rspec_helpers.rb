1bac24c71c727738827721f0caca174cfcfdfce9
Jordi Noguera <jordi@pivotallabs.com>
2014-05-15 17:57:21 -0400

    Refactor setting api before suite code

--- lib/machete/rspec_helpers.rb
+++ lib/machete/rspec_helpers.rb
@@ -6,17 +6,15 @@ RSpec.configure do |config|
     if Machete::BuildpackMode.offline?
       Machete.logger.action 'Selecting OFFLINE BOSH Lite'
 
-      offline_api_target = ENV['OFFLINE_API_TARGET'] ? ENV['OFFLINE_API_TARGET'] : 'api.10.245.0.34.xip.io'
-      Machete.logger.info `cf api #{offline_api_target} --skip-ssl-validation`
+      api_target = ENV['OFFLINE_API_TARGET'] || 'api.10.245.0.34.xip.io'
     else
       Machete.logger.action 'Selecting ONLINE BOSH Lite'
 
-      online_api_target = ENV['ONLINE_API_TARGET'] ? ENV['ONLINE_API_TARGET'] : 'api.10.244.0.34.xip.io'
-      Machete.logger.info `cf api #{online_api_target} --skip-ssl-validation`
+      api_target = ENV['ONLINE_API_TARGET'] || 'api.10.244.0.34.xip.io'
     end
 
+    Machete.logger.info `cf api #{api_target} --skip-ssl-validation`
     Machete.logger.action 'Logging into BOSH lite'
-
     Machete.logger.info `cf login -u admin -p admin`
     Machete.logger.info `cf target -o pivotal -s integration`
   end
