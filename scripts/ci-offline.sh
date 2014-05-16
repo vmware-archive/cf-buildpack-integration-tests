21e332fa4570428623d59cc70439b96b27b1a6ce
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-05-16 14:11:35 -0400

    offline and online CI scripts

--- scripts/ci-offline.sh
+++ scripts/ci-offline.sh
@@ -6,21 +6,18 @@ set -e
 ./bin/package offline
 
 # Create buildpack on CF
-cf api api.10.245.0.34.xip.io
+cf api api.10.245.0.34.xip.io --skip-ssl-validation
 cf login -u admin -p admin -o pivotal -s integration
-cf create-buildpack $language-test-buildpack $language_buildpack.zip 1 --enable
+cf delete-buildpack ${language}-test-buildpack -f
+cf create-buildpack ${language}-test-buildpack ${language}_buildpack.zip 1 --enable
+
+# RVM
+export PATH=/Users/pivotal/.rvm/bin:$PATH
+rvm use default
 
 # Run specs
-
-echo
-echo "******* CI ***************************************************"
-echo "******* Adding RVM and using Ruby 2.0"
-export PATH=/Users/pivotal/.rvm/bin:$PATH
-rvm use ruby-2.0.0-p451
-
 BUNDLE_GEMFILE=cf.Gemfile bundle
 BUNDLE_GEMFILE=cf.Gemfile BUILDPACK_MODE=offline rspec cf_spec
 
 # Release buildpack
-mkdir -p release
-mv $language_buildpack.zip release/$language_buildpack_$BUILD_NUMBER_offline.zip
+mv ${language}_buildpack.zip /tmp/${language}_buildpack_${BUILD_NUMBER}_offline.zip
--- scripts/ci-online.sh
+++ scripts/ci-online.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
+#!/bin/bash -l
 
 set -e
 
+# Build online package
+./bin/package online
+
+# Create buildpack on CF
+cf api api.10.244.0.34.xip.io --skip-ssl-validation
+cf login -u admin -p admin -o pivotal -s integration
+cf delete-buildpack ${language}-test-buildpack -f
+cf create-buildpack ${language}-test-buildpack ${language}_buildpack.zip 1 --enable
+
+# RVM
+export PATH=/Users/pivotal/.rvm/bin:$PATH
+rvm use default
+
+# Run specs
 BUNDLE_GEMFILE=cf.Gemfile bundle
 BUNDLE_GEMFILE=cf.Gemfile rspec cf_spec
+
+# Release buildpack
+mv ${language}_buildpack.zip /tmp/${language}_buildpack_${BUILD_NUMBER}_online.zip
