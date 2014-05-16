a7226f07582bb96a9122b723d8b5969010aba492
Chris Haueter and Rasheed Abdul-Aziz <pair+chaueter+squeedee@pivotallabs.com>
2014-05-16 14:02:18 -0400

    Extract system helpers from firewall

--- lib/machete/firewall.rb
+++ lib/machete/firewall.rb
@@ -1,5 +1,10 @@
+require 'machete/system_helper'
+
 module Machete
   module Firewall
+
+    extend Machete::SystemHelper
+
     class << self
       def disable_firewall
         restore_iptables
@@ -51,42 +56,6 @@ module Machete
 
       def dns_addr
         @dns_addr ||= run_on_host("sudo ip -f inet addr | grep eth0 | grep inet").split(" ")[1].gsub(/\d+\/\d+$/, "0/24")
-      end
-
-      def vagrant_cwd
-        return @vagrant_cwd if @vagrant_cwd
-
-        if ENV['VAGRANT_CWD']
-          @vagrant_cwd = ENV['VAGRANT_CWD']
-        else
-          Machete.logger.error "No VAGRANT_CWD. You probably want:\nVAGRANT_CWD=$HOME/workspace/bosh-lite\nor\nVAGRANT_CWD=$HOME/workspace/bosh-lite-2nd-instance"
-        end
-      end
-
-      def set_vagrant_working_directory
-        # this is local to the clean env - thats why it seems strange that we set it often.
-        ENV['VAGRANT_CWD'] = vagrant_cwd
-      end
-
-      def run_on_host(command)
-        if in_gocd?
-          `ssh -i /var/vcap/jobs/gocd_agent/id_rsa_bosh_lite ubuntu@10.10.48.64 -c "#{command}" 2>&1`
-        else
-          with_vagrant_env { `vagrant ssh -c "#{command}" 2>&1` }
-        end
-      end
-
-      def in_gocd?
-        return @in_gocd unless @in_gocd.nil?
-
-        @in_gocd = File.exists?("/var/vcap/jobs/gocd_agent/id_rsa_bosh_lite")
-      end
-
-      def with_vagrant_env
-        Bundler.with_clean_env do
-          set_vagrant_working_directory
-          yield
-        end
       end
 
       def select_default_masquerade_rules(rules)
