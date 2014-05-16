d5402d59e2530f2dc2e0449da5e94abeee164136
Jordi Noguera <jordi@pivotallabs.com>
2014-05-16 11:13:04 -0400

    ci offline script

--- scripts/ci-offline.sh
+++ scripts/ci-offline.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+# Build offline package
+./bin/package offline
+
+# Creat buildpack on CF
+cf create-buildpack $language-test-buildpack $language_buildpack.zip 1 --enable
+
 BUNDLE_GEMFILE=cf.Gemfile bundle
 BUNDLE_GEMFILE=cf.Gemfile BUILDPACK_MODE=offline rspec cf_spec
+
+mkdir -p release
+
+mv $language_buildpack.zip release/$language_buildpack_$BUILD_NUMBER_offline.zip
