84dc1bff367368f5fadf1811b4a3a789166a8b3f
Aaron Triantafyllidis and Eric Hu <pair+atriantafyllidis+ehu@pivotallabs.com>
2014-05-20 10:10:35 -0400

    Remove firewall spec and associated test applications

--- test_applications/null/app_direct_app/README.md
+++
@@ -1,6 +0,0 @@
-firewall test default
-=====================
-
-An empty app that uses whatever ruby is on a droplet to hold $PORT open.
-In short, it keeps the app marked as running.
-
--- test_applications/null/app_direct_app/exec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-header=`curl $APPDIRECT_URL -i | grep HTTP`
-
-if [ "$header" == "" ]; then
-  echo "-----> Not able to connect to AppDirect"
-else
-  echo "-----> Connected to AppDirect"
-fi
