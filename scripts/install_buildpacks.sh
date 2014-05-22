b2de425d9af27e6341977768d5e36a3c2e2c43c4
Aaron Triantafyllidis <pair+atriantafyllidis@pivotallabs.com>
2014-05-22 12:18:06 -0400

    Remove install buildpacks script

--- scripts/install_buildpacks.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/cloudfoundry/cf-buildpack-ruby ../buildpacks/cf-buildpack-ruby
-git clone https://github.com/cloudfoundry-incubator/cf-buildpack-go ../buildpacks/cf-buildpack-go
-git clone https://github.com/cloudfoundry/cf-buildpack-nodejs ../buildpacks/cf-buildpack-nodejs
-git clone https://github.com/cf-buildpacks/cf-buildpack-null ../buildpacks/cf-buildpack-null
-git clone https://github.com/cloudfoundry-incubator/cf-buildpack-python ../buildpacks/cf-buildpack-python
